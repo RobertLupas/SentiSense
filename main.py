426b3af43233442dbcfc232f402f31220a61b0c4
Robert Lupas <robertlupas09@gmail.com>
2023-12-12 19:36:33 +0200

    Moved model training to another file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import pickle
 
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
@@ -6,34 +7,17 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 from text_processing import preprocess_dataframe
+from train_model import train_save_model
 
-# Load the JSON file into a Python list
-with open('dataset.json', 'r') as file:
-  data = json.load(file)
-
-# Convert the data to a Pandas DataFrame for easier manipulation
-df = pd.DataFrame(data)
-
-# Preprocess DataFrame using the function from text_processing module
-df = preprocess_dataframe(df)
-
-# Vectorize text data using Bag-of-Words (BoW)
-vectorizer = CountVectorizer()  # Or use TfidfVectorizer()
-X = vectorizer.fit_transform(df['Text'])
-
-# Split data into Features (X) and Labels (y)
-y = df['Sentiment']
-
-# Train-Test Split
-X_train, X_test, y_train, y_test = train_test_split(X,
-                                                    y,
-                                                    test_size=0.2,
-                                                    random_state=42)
-
-# Build and Train the Sentiment Analysis Model (Example: Logistic Regression)
-model = LogisticRegression()
-model.fit(X_train, y_train)
-
-# Evaluate Model
-accuracy = model.score(X_test, y_test)
-print("Model Accuracy:", accuracy)
+# Check if the trained model exists
+try:
+    with open('trained_model.pkl', 'rb') as model_file:
+        model = pickle.load(model_file)
+except FileNotFoundError:
+    print("Training model...")
+    
+    # Call the function from the train_model module to train and save the model
+    train_save_model('dataset.json', 'trained_model.pkl')
+    # Load the newly trained model
+    with open('trained_model.pkl', 'rb') as model_file:
+        model = pickle.load(model_file)
---
+++ train_model.py
@@ -0,0 +1,45 @@
+import json
+import pickle
+
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+
+from text_processing import preprocess_dataframe
+
+
+def train_save_model(data_file, model_store_file):
+    # Load the JSON file into a Python list
+    with open(data_file, 'r') as file:
+        data = json.load(file)
+
+    # Convert the data to a Pandas DataFrame for easier manipulation
+    df = pd.DataFrame(data)
+
+    # Preprocess DataFrame using the function from text_processing module
+    df = preprocess_dataframe(df)
+
+    # Vectorize text data using Bag-of-Words (BoW)
+    vectorizer = CountVectorizer()
+    X = vectorizer.fit_transform(df['Text'])
+
+    # Split data into Features (X) and Labels (y)
+    y = df['Sentiment']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Build and Train the Sentiment Analysis Model (Example: Logistic Regression)
+    model = LogisticRegression()
+    model.fit(X_train, y_train)
+
+    # Evaluate Model
+    accuracy = model.score(X_test, y_test)
+    print("Model Accuracy:", accuracy)
+
+    # Save the trained model to a file
+    with open(model_store_file, 'wb') as model_store_file:
+        pickle.dump(model, model_store_file)
+
+# Execute training and model saving if this script is directly run
+if __name__ == "__main__":
+    print("Error. This file needs to be run from another file, with necessary arguments.")
